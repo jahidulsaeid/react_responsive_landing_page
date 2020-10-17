cc5811364fff106a2a8da4085b943fa9d6a905b1
jahidulsaeid <muhammadsaeid0@gmail.com>
2020-10-17 20:53:52 +0600

    full project(fix all image url)

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,13 +1,35 @@
-import React from 'react'
+import React from 'react';
+import './Button.css';
 
-const Button = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+const STYLES = ['btn--primary', 'btn--outline'];
 
-// https://www.youtube.com/watch?v=3nLTB_E6XAM (Time: 32:31)
+const SIZES = ['btn--medium', 'btn--large', 'btn--mobile', 'btn--wide'];
 
-export default Button
+const COLOR = ['primary', 'blue', 'red', 'green'];
+
+export const Button = ({
+  children,
+  type,
+  onClick,
+  buttonStyle,
+  buttonSize,
+  buttonColor
+}) => {
+  const checkButtonStyle = STYLES.includes(buttonStyle)
+    ? buttonStyle
+    : STYLES[0];
+
+  const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
+
+  const checkButtonColor = COLOR.includes(buttonColor) ? buttonColor : null;
+
+  return (
+    <button
+      className={`btn ${checkButtonStyle} ${checkButtonSize} ${checkButtonColor}`}
+      onClick={onClick}
+      type={type}
+    >
+      {children}
+    </button>
+  );
+};
